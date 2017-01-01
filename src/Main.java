79eef66a4d75ce6d7c49edc012991b8ea974c26e
Davis Crain <dhcrain@gmail.com>
2017-01-01 15:03:04 -0500

    able to print histogram graph of results

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,17 @@ public class Main {
 
             TxtToList wordList = new TxtToList(new File(args[0]));
 
-//            System.out.printf("Word counts: %s, %n", wordList.getWordCount());
-//            System.out.printf("Sorted Word counts: %s, %n", wordList.sortByValue(wordList.getWordCount()));
-
             wordList.removeCommonWords();
-            
+
             wordList.countWords();
 
-            int count = 0;
-            for (WordCount word : wordList.getWordCountList(1, 20)) {
-                count++;
-                System.out.printf("%d.) %s  %n", count, word);
-            }
+//            int count = 0;
+//            for (WordCount word : wordList.getWordCountList(1, 20)) {
+//                count++;
+//                System.out.printf("%-2d) %s  %n", count, word);
+//            }
+
+            wordList.printWordGraph();
 
         }
 
--- src/TxtToList.java
+++ src/TxtToList.java
@@ -42,32 +42,30 @@ public class TxtToList {
     }
 
     public void removeCommonWords() {
-
-        // defining an array of stop words (you probably want that as a constant somewhere else)
         final String[] stopWords = {"a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your"};
-
-        // printing un-processed list
-//        System.out.printf("Dirty: %s%n", this.mWordList);
-
-        // invoking removeAll to remove all stop words
         this.mWordList.removeAll(Arrays.asList(stopWords));
-
-        // printing "clean" list
-//        System.out.printf("Clean: %s%n", this.mWordList);
-
-//        commonWords =
-//
-//        for (String word : commonWords) {
-
-//        }
     }
 
+    public void printWordGraph() {
+        List<WordCount> wordCountList = getWordCountList(1, 20);
+        double graphMax = (double) wordCountList.get(0).getCount();
+        int rank = 0;
+        for (WordCount wordCount : wordCountList) {
+            rank++;
+            String word = wordCount.getWord();
+            Integer count = wordCount.getCount();
+            // get 1st item, scale the graph based on that number
+            Double scale = (count * (50.0 / graphMax));
+//            System.out.println(scale.intValue());
+            String graph = new String(new char[scale.intValue()]).replace("\0", "#");
+            System.out.printf("%-2d) %-13s:\t %s (%d) %n", rank, word, graph, count);
+        }
+    }
 
     public void countWords() {
 
         this.mWordCountList = new ArrayList<>();
         Map<String, Integer> wordCounts = new HashMap<>();
-        WordCount newWord;
         Integer count;
 
         for (String word : mWordList) {
@@ -99,45 +97,6 @@ public class TxtToList {
         return mWordCountList;
     }
 
-//    // from http://stackoverflow.com/questions/109383/sort-a-mapkey-value-by-values-java
-//    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
-//        return map.entrySet()
-//                .stream()
-//                .sorted(Map.Entry.comparingByValue(/*Collections.reverseOrder()*/))
-//                .collect(Collectors.toMap(
-//                        Map.Entry::getKey,
-//                        Map.Entry::getValue,
-//                        (e1, e2) -> e1,
-//                        LinkedHashMap::new
-//                ));
-//    }
-
-    //    public Map<String, Integer> getTopWords() {
-//        Map<String, Integer> wordCount = this.getWordCount();
-//
-//        ArrayList<Integer> values = new ArrayList<>();
-//        values.addAll(wordCount.values()); // Array of counts only
-//
-//        Collections.sort(values, Collections.reverseOrder()); // sorts counts high to low
-////        System.out.print(wordCount);
-//
-//        int lastCount = -1;
-//        int rank = 1;
-//
-//        for (Integer count : values.subList(0, 20)) {
-//            if (lastCount == count) { // without duplicates
-//                continue;
-//            }
-//            lastCount = count;
-//
-//            for (String word : wordCount.keySet()) {
-//                if (wordCount.get(word) == lastCount) { // which have this value
-//                    System.out.printf("%d.) %s - %s  %n", rank, word, lastCount);
-//                }
-//            }
-//            rank++;
-//        }
-//        return wordCount;
 
 }
 
